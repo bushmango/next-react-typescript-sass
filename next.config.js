83c8efc218389995b3fea300c80a6c50ea43d466
Unknown <bushmango@gmail.com>
2019-02-16 14:59:52 -0600

    add serverless now deploy

--- next.config.js
+++ next.config.js
@@ -3,13 +3,26 @@ const withSass = require('@zeit/next-sass')
 const withTypescript = require('@zeit/next-typescript')
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
 
-module.exports = withTypescript(
-  withSass({
-    webpack (config, options) {
-      // Do not run type checking twice:
-      if (options.isServer) { config.plugins.push(new ForkTsCheckerWebpackPlugin()) }
-      return config
-    },
-    cssModules: true
-  })
-)
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
+
+module.exports = (phase, { defaultConfig }) => {
+  let config = withTypescript(
+    withSass({
+      webpack(config, options) {
+        // Do not run type checking twice:
+        if (options.isServer) {
+          config.plugins.push(new ForkTsCheckerWebpackPlugin())
+        }
+        return config
+      },
+      cssModules: true,
+    })
+  )
+
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return config
+  } else {
+    config.target = 'serverless'
+    return config
+  }
+}
